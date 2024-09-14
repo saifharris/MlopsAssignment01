6e4a05f94004067aeab9a4f4e2f9a860424412d6
saifharris <163036184+saifharris@users.noreply.github.com>
2024-09-14 21:16:47 +0500

    Update test.py
    flake8 fix

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-
 import unittest
 from app import app
 
 class FlaskTestCase(unittest.TestCase):
+
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
@@ -24,6 +24,7 @@ class FlaskTestCase(unittest.TestCase):
             'LSTAT': 5.0
         })
         self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
